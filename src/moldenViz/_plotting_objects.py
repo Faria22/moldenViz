55416e4e1a90afac284f45a0a1b12e4d6961335c
Faria22 <fariafelipe22@hotmail.com>
2025-08-03 20:20:45 -0400

    Added config options and allowed for split coloring of bonds

--- src/moldenViz/_plotting_objects.py
+++ src/moldenViz/_plotting_objects.py
@@ -1,15 +1,14 @@
-import json
 import logging
 from dataclasses import dataclass
-from pathlib import Path
+from enum import Enum
 from typing import cast
 
 import numpy as np
 import pyvista as pv
-from _config_module import load_atom_types
 from numpy.typing import NDArray
 from scipy.spatial.distance import pdist, squareform
 
+from ._config_module import Config
 from .parser import _Atom
 
 logger = logging.getLogger(__name__)
@@ -20,17 +19,13 @@ class AtomType:
     name: str
     color: str
     radius: float
-    max_bonds: int
+    max_num_bonds: int
 
 
-# Atom type based on charge
-# Colors are based on CPK color scheme
-# https://sciencenotes.org/molecule-atom-colors-cpk-colors/
-with Path('atoms.json').open('r') as f:
-    atom_types = json.load(f)
+config = Config()
 
-ATOM_TYPES = {int(k): AtomType(**v) for k, v in atom_types.items()}
-ATOM_TYPES = load_atom_types()
+
+ATOM_TYPES = config.atom_types
 
 # Default atom type for invalid atomic numbers
 ATOM_X = AtomType('X', 'black', 1, 0)
@@ -55,30 +50,65 @@ class Atom:
 
     def remove_extra_bonds(self) -> None:
         """Remove the longest bonds if there are more bonds than `max_bonds`."""
-        if len(self.bonds) <= self.atom_type.max_bonds:
+        if len(self.bonds) <= self.atom_type.max_num_bonds:
             return
 
         self.bonds.sort(key=lambda x: x.length)
 
-        for bond in self.bonds[self.atom_type.max_bonds :]:
+        for bond in self.bonds[self.atom_type.max_num_bonds :]:
             bond.mesh = None
 
 
 class Bond:
-    def __init__(self, atom_a: Atom, atom_b: Atom, radius: float = 0.15) -> None:
+    class ColorType(Enum):
+        UNIFORM = 'uniform'
+        SPLIT = 'split'
+
+    def __init__(self, atom_a: Atom, atom_b: Atom) -> None:
+        bond_vec = atom_a.center - atom_b.center
         center = (atom_a.center + atom_b.center) / 2
 
-        bond_vec = atom_a.center - atom_b.center
         length = cast(float, np.linalg.norm(bond_vec))
-
         self.length = length
 
-        self.mesh = pv.Cylinder(radius=radius, center=center, height=length, direction=bond_vec)
+        if config.molecule.bond.color_type.lower() == self.ColorType.UNIFORM.value:
+            self.mesh = pv.Cylinder(
+                radius=config.molecule.bond.radius,
+                center=center,
+                height=length,
+                direction=bond_vec,
+            )
+            self.color = config.molecule.bond.color
+        elif config.molecule.bond.color_type.lower() == self.ColorType.SPLIT.value:
+            center = atom_a.center + atom_b.center
+            center_a = center / 4
+            center_b = center * 3 / 4
+
+            mesh_a = pv.Cylinder(
+                radius=config.molecule.bond.radius,
+                center=center_a,
+                height=length / 2,
+                direction=bond_vec,
+            )
+
+            mesh_b = pv.Cylinder(
+                radius=config.molecule.bond.radius,
+                center=center_b,
+                height=length / 2,
+                direction=bond_vec,
+            )
+
+            self.mesh = [mesh_a, mesh_b]
+            self.color = [atom_a.atom_type.color, atom_b.atom_type.color]
+        else:
+            raise ValueError(
+                f'Invalid bond color type: {config.molecule.bond.color_type}. '
+                f'Expected one of {[color_type.value for color_type in self.ColorType]}.',
+            )
+
         self.atom_a = atom_a
         self.atom_b = atom_b
 
-        self.color = 'grey'
-        self.type = 'uniform'
         self.plotted = False
 
     def trim_ends(self) -> None:
@@ -86,9 +116,17 @@ class Bond:
         if self.mesh is None:
             return
 
-        self.mesh = self.mesh.triangulate() - self.atom_a.mesh - self.atom_b.mesh
+        warning = False
+        if isinstance(self.mesh, list):
+            self.mesh = [mesh.triangulate() - atom.mesh for mesh, atom in zip(self.mesh, [self.atom_a, self.atom_b])]
+            if any(mesh.n_points == 0 for mesh in self.mesh):
+                warning = True
+        else:
+            self.mesh = self.mesh.triangulate() - self.atom_a.mesh - self.atom_b.mesh
+            if self.mesh.n_points == 0:
+                warning = True
 
-        if self.mesh.n_points == 0:
+        if warning:
             logger.warning(
                 'Error: Bond mesh is empty between atoms %s and %s.',
                 self.atom_a.atom_type.name,
@@ -98,10 +136,7 @@ class Bond:
 
 
 class Molecule:
-    def __init__(self, atoms: list[_Atom], max_bond_length: float = 4) -> None:
-        # Max_bond_length helps the program skip over any bonds that should not exist
-        self.max_bond_length = max_bond_length
-
+    def __init__(self, atoms: list[_Atom]) -> None:
         # Max radius is used later for plotting
         self.max_radius = 0
 
@@ -115,7 +150,7 @@ class Molecule:
 
         distances = squareform(pdist(atom_centers))  # Compute pairwise distances
         mask = np.triu(np.ones_like(distances, dtype=bool), k=1)  # Ensure boolean mask
-        indices = np.where((distances < self.max_bond_length) & mask)  # Apply mask
+        indices = np.where((distances < config.molecule.max_bond_lenght) & mask)  # Apply mask
 
         for atom_a_ind, atom_b_ind in zip(indices[0], indices[1]):
             bond = Bond(self.atoms[atom_a_ind], self.atoms[atom_b_ind])
@@ -125,16 +160,20 @@ class Molecule:
         for atom in self.atoms:
             atom.remove_extra_bonds()
 
-    def add_meshes(self, plotter: pv.Plotter, opacity: float = 1) -> list[pv.Actor]:
+    def add_meshes(self, plotter: pv.Plotter, opacity: float = config.molecule.opacity) -> list[pv.Actor]:
         actors = []
         for atom in self.atoms:
-            actors.append(plotter.add_mesh(atom.mesh, color=atom.atom_type.color, smooth_shading=True))
+            actors.append(plotter.add_mesh(atom.mesh, color=atom.atom_type.color, smooth_shading=config.smooth_shading))
             for bond in atom.bonds:
                 if bond.plotted or bond.mesh is None:
                     continue
 
                 bond.trim_ends()
-                actors.append(plotter.add_mesh(bond.mesh, color=bond.color, opacity=opacity))
+                if isinstance(bond.mesh, list):
+                    for mesh, color in zip(bond.mesh, bond.color):
+                        actors.append(plotter.add_mesh(mesh, color=color, opacity=opacity))
+                else:
+                    actors.append(plotter.add_mesh(bond.mesh, color=bond.color, opacity=opacity))
                 bond.plotted = True
 
         return actors
