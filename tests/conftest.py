b9ed2482f2a7fd2da9135a62aa9a43cd40d8dc9c
Faria22 <fariafelipe22@hotmail.com>
2026-08-07 08:04:56 -0500

    Configure Qt tests for headless runners

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,11 @@
 
 from __future__ import annotations
 
+import os
 import sys
 from pathlib import Path
+
+os.environ.setdefault('QT_QPA_PLATFORM', 'offscreen')
 
 _REPO_ROOT = Path(__file__).resolve().parents[1]
 _SRC = _REPO_ROOT / 'src'
